a36b7f6630807a1578b1201b5ad692a505993356
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-02-26 09:41:46 -0700

    drill fix

--- practice3.js
+++ practice3.js
@@ -12,37 +12,79 @@
  */
 
 
-function parentheses(string) {
+// function parentheses(string) {
+//   let stack = [];
+//   let map = {
+//     '(':')',
+//     '{': '}',
+//     '[': ']',
+//   };
+
+//   for (let i=0; i< string.length; i++) {
+//     //push all the open parenthesis types into stack
+//     if (string[i] === '(' || string[i] === '{' || string[i] === '[') {
+//       stack.push(string[i]);
+//     }
+//     //if we reach a closing bracket, we need to make sure the last parenthesis before it was an open bracket.
+//     //check the last item in the stack to see if it matches the closing bracket type.
+//     if (string[i] === ')' || string[i] === '}' || string[i] === ']') {
+//       let last = stack.pop();
+//       if(map[last] !== string.charAt(i)){
+//         return `You are missing ${map[last]}`;
+//       }
+//     }
+//   }
+//   //if any extra parenthesis remain in the stack that means there is an uneven num of parenthesis and therefore
+//   //we are missing a parenthesis somewhere.
+//   if(stack.length !== 0){
+//     let last = stack.pop();
+//     return `You are missing ${map[last]}`;
+//   }
+// return 'You have all the parentheses';
+// }
+
+// console.log(parentheses('(){}'));
+// console.log(parentheses('[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]'));
+// console.log(parentheses('({(()))}}'));
+
+
+//repeat drill
+
+
+function parenthesis(string){
   let stack = [];
-  let map = {
-    '(':')',
-    '{': '}',
+  let key = {
+    '(': ')',
     '[': ']',
+    '{': '}'
   };
 
-  for (let i=0; i< string.length; i++) {
-    //push all the open parenthesis types into stack
-    if (string[i] === '(' || string[i] === '{' || string[i] === '[') {
+  for(let i=0; i<string.length; i++){
+    if(string[i] === '(' || string[i]==='{' || string[i]==='['){
       stack.push(string[i]);
     }
-    //if we reach a closing bracket, we need to make sure the last parenthesis before it was an open bracket.
-    //check the last item in the stack to see if it matches the closing bracket type.
-    if (string[i] === ')' || string[i] === '}' || string[i] === ']') {
+
+    if(string[i] === ')' || string[i]==='}' || string[i] ===']'){
       let last = stack.pop();
-      if(map[last] !== string.charAt(i)){
-        return `You are missing ${map[last]}`;
+      if(!last){
+        return `You have an extra ${string[i]}`;
+      }
+
+      if(key[last] !== string[i]){
+        return `You are missing ${key[last]}`;
       }
     }
   }
-  //if any extra parenthesis remain in the stack that means there is an uneven num of parenthesis and therefore
-  //we are missing a parenthesis somewhere.
+
   if(stack.length !== 0){
     let last = stack.pop();
-    return `You are missing ${map[last]}`;
+    return `You are missing ${key[last]}`;
   }
-return 'You have all the parentheses';
+
+  return 'All parentheses present';
 }
 
-console.log(parentheses('(){}'));
-console.log(parentheses('[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]'));
-console.log(parentheses('({(()))}}'));
+console.log(parenthesis('(){}]'));
+console.log(parenthesis('[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]'));
+console.log(parenthesis('({(()))}}'));
+console.log(parenthesis(')({(()))}}('));
