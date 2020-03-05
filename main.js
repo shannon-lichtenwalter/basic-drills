1d010a71c2edc9f402f1718c40ebc3c6e2812570
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-03-05 15:52:59 -0700

    minor changes

--- main.js
+++ main.js
@@ -19,24 +19,24 @@ function submitForm(event) {
 }
 
 function getTempData(zipcode) {
-  console.log(zipcode);
   const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=4904dcdd9c11a68c767a9aa435ae14db`;
   return fetch(url, {
     method: 'GET'
   })
-    .then(res => res.json())
+    .then(res =>
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    )
     .then(res => {
-      console.log(res);
       const city = res.name;
       const kelvin = res.main.temp;
       const sky = res.weather[0].description;
-      console.log(city);
-      console.log(kelvin);
-      console.log(sky);
-      document.getElementById('cityName').insertAdjacentHTML('beforeend', `Checkout the weather in: ${city}`);
+      document.getElementById('cityName').insertAdjacentHTML('beforeend', `Check out the weather in: ${city}`);
       document.getElementById('kelvin').insertAdjacentHTML('beforeend', `Temp: ${Math.floor(kelvin)} degrees kelvins`);
       document.getElementById('celcius').insertAdjacentHTML('beforeend', `Temp: ${Math.floor(kelvin - 273.15)} degrees celcius`);
       document.getElementById('farenheit').insertAdjacentHTML('beforeend', `Temp: ${Math.floor(kelvin * (9 / 5) - 459.67)} degrees farenheit`);
       document.getElementById('sky').insertAdjacentHTML('beforeend', `Sky looks: ${sky}`);
-    });
+    })
+    .catch(e => document.getElementById('cityName').insertAdjacentHTML('beforeend', `Oops! <span class='capitalize'>${e.message}</span>. Please Try Another Zipcode`));
 }
